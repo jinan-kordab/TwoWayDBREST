a4c9ed2bb16fb15eb0e652259c54ac3b59658b25
JINAN <JINAN@JINAN-PC>
2018-05-17 20:18:34 -0400

    Add project files.

---
+++ TwoWayDBREST/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TwoWayDBREST.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public string Get()
+        {
+            List<EntityFramework.UserInfo> returnedList = new List<EntityFramework.UserInfo>();
+            using (EntityFramework.TwoWayDataBindingEntities entities = new EntityFramework.TwoWayDataBindingEntities())
+            {
+                var r = (from q in entities.UserInfoes
+                         select q).ToList();
+                returnedList = r;
+            }
+            string output = JsonConvert.SerializeObject(returnedList);
+            return output;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public JObject Post([FromBody]EntityFramework.UserInfo newuser)
+        {
+            EntityFramework.UserInfo newUInfo = new EntityFramework.UserInfo
+            {
+                 UserDayPhone = newuser.UserDayPhone,
+                 UserStreet = newuser.UserStreet,
+                 UserApartment = newuser.UserApartment,
+                 UserCity = newuser.UserCity,
+                 UserProvince = newuser.UserProvince,
+                 UserPostalCode = newuser.UserPostalCode,
+                 UserEmail = newuser.UserEmail
+            };
+
+            using (EntityFramework.TwoWayDataBindingEntities entities = new EntityFramework.TwoWayDataBindingEntities())
+            {
+                entities.UserInfoes.Add(newUInfo);
+                entities.SaveChanges();
+                var jObject = JObject.Parse("{\"status\": \"success\",\"data\": \"OK\",\"message\": null }");
+                return jObject;
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TwoWayDBREST/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TwoWayDBREST
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            //GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+        
+        
+        }
+
+        protected void Application_BeginRequest()
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                //These headers are handling the "pre-flight" OPTIONS call sent by the browser
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "POST,GET, OPTIONS");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+        }
+    }
+}
